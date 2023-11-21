557d88b4c087cd3d203f0a36975f5448061b0eeb
NBK-01 <nayefkanaan03@gmail.com>
2023-11-21 19:43:55 +0200

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex items-center">
+        <h1 className="mx-auto"> Test </h1>
+      </div>
+    </>
+  );
+}
