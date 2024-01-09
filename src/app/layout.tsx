c0c7abf38d8145ed56675836f97990f90c258efd
NBK-01 <nayefkanaan03@gmail.com>
2024-01-09 19:04:10 +0200

    minor: header name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar} from "@/components/layout/nav.components";
+import { Navbar } from "@/components/layout/nav.components";
 import { ThemeProvider, UIProvider } from "@/components/layout/providers";
-import {Toaster} from "sonner"
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "excelsior",
+  title: "Nayef Kanaan",
   description: "A showcase of Nayef Kanaan's work, projects, and profile",
 };
 
@@ -22,17 +22,16 @@ export default function RootLayout({
       <body className="overflow-hidden" style={inter.style}>
         <UIProvider>
           <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toaster position="top-center" richColors/>
-          <Navbar />
-          {children}
-        </ThemeProvider>
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster position="top-center" richColors />
+            <Navbar />
+            {children}
+          </ThemeProvider>
         </UIProvider>
-
       </body>
     </html>
   );
