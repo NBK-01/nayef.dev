be6488ca36a8ac60e601ee9c421cc2043d8bb6b3
NBK-01 <nayefkanaan03@gmail.com>
2024-01-11 18:52:22 +0200

    bug: broken link

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,10 +6,12 @@ import React from 'react'
 const About = () => {
   return (
    <section className="max-w-screen-xl mx-auto flex pt-10 justify-between px-16">
-        <div className="w-[65%] px-6 space-y-8">
+        <div className="w-[65%] px-6 space-y-9">
             <h1 className="italic text-xl"> a little bit about me </h1>
+
             <p className="text-neutral-600"> i'm a frontend a focused fullstack developer currently focused on open-source and contributing to the community. i'm working on projects like <Link href="https://prbly.xyz" className="text-green-700 italic underline"> prbly </Link>, <Link href="https://kiwii.app" className="text-green-700 italic underline"> kiwii </Link>, and much more coming on the way </p>
-            <p className="text-neutral-600"> over the past couple years i've built landing pages for clients and startups and worked on my own products and applications; while also dipping my toes in creative coding, pure frontend, and open source. i mainly build fullstack apps now with <Link href="https://github.com/vercel/next.js" className="italic underline"> nextjs </Link> , <Link href="https://github.com/facebook/react" className="text-green-700 italic underline"> react </Link> ,  <Link href="https://tailwindcss.com/" className="text-green-700 italic underline"> tailwind </Link>, and <Link href="https://github.com/vercel" className="text-green-700 italic underline"> vercel </Link> </p>
+
+            <p className="text-neutral-600"> over the past couple years i've built landing pages for clients and startups and worked on my own products and applications; while also dipping my toes in creative coding, pure frontend, and open source. i mainly build fullstack apps now with <Link href="https://github.com/vercel/next.js" className="text-green-700 italic underline"> nextjs </Link> , <Link href="https://github.com/facebook/react" className="text-green-700 italic underline"> react </Link> ,  <Link href="https://tailwindcss.com/" className="text-green-700 italic underline"> tailwind </Link>, and <Link href="https://github.com/vercel" className="text-green-700 italic underline"> vercel </Link> </p>
         </div>
 
 
